5f051d75add4901e103e8c8a29632ca6d3bbabf9
JosiasAurel <ndjosiasaurel@gmail.com>
2021-12-22 12:30:57 +0100

    mock homepage

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,16 +2,28 @@ import React from "react";
 
 import { Tag } from "@geist-ui/react";
 
+import Link from "next/link";
+
 import styles from "../styles/components.module.css";
 
 const Header = () => {
     return (
-        <header>
-            <div>
-                <Tag>
+        <header className={styles.header}>
+            <div style={{
+                display: "flex",
+                justifyContent: "space-event",
+                alignItems: "center"
+            }}>
+                <Tag type="success">
                     KubeHacks : Init
                 </Tag>
             </div>
+            <nav>
+                <a>Home</a>
+                <a>Register</a>
+                <a>Sign In</a>
+                <a>Organizers</a>
+            </nav>
         </header>
     )
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,47 @@
 import React from "react";
 
-import { Card } from "@geist-ui/react";
+import styles from "../styles/index.module.css";
+
+import { event2021HeroBlurredDataURI } from "../misc";
+
+import { Button, Modal } from "@geist-ui/react";
 
 import Header from "../components/Header";
 
+import Image from "next/image";
+
 const HomePage = () => {
+    const [registratioModal, setRegistrationModal] = React.useState(false);
+    const [menu, setMenu] = React.useState(false);
+
     return (
-        <div>
+        <>
             <Header />
-            <h1>KubeHacks</h1>
-        </div>
+            <main className={styles.content}>
+                <div style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-evenly",
+                    alignItems: "center"
+                }}>
+                    <Image placeholder="blur" blurDataURL={event2021HeroBlurredDataURI} src="/event2021.png" width={600} height={400} />
+                    <h2 style={{
+                        textDecoration: "underline"
+                    }}>KubeHacks Init 2021</h2>
+                    <Button type="success-light">
+                        Register
+                    </Button>
+                </div>
+            </main>
+            <Modal visible={menu} onClose={() => setMenu(false)}>
+                    <Modal.Title>
+                        Menu
+                    </Modal.Title>
+                    <Modal.Content>
+                        Some Menu Items
+                    </Modal.Content>
+            </Modal>
+        </>
     )
 }
 
