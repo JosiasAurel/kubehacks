6e79240c1c7e41178cd1689ac2005e76c077b101
JosiasAurel <ndjosiasaurel@gmail.com>
2021-12-22 13:48:46 +0100

    add footer

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,7 @@ import styles from "../styles/index.module.css";
 
 import { event2021HeroBlurredDataURI } from "../misc";
 
-import { Button, Modal } from "@geist-ui/react";
+import { Button, Modal, Spacer, Divider } from "@geist-ui/react";
 
 import Header from "../components/Header";
 import Organizer from "../components/Organizer";
@@ -58,6 +58,11 @@ const HomePage = () => {
             submit for testing. Each test passed is an additional point for the
             participant.
           </p>
+
+          <p>
+              All participants who succeed in completing at least 3 tasks will receive
+              a certificate of completion in reward of their hard work.
+          </p>
         </div>
         <div id="organizers">
           <h2 style={{ textAlign: "center" }}>Organizers</h2>
@@ -76,7 +81,48 @@ const HomePage = () => {
             />
           </div>
         </div>
+
+        <div style={{ textAlign: "center",
+         }}>
+            <h2>Sponsors</h2>
+            <p>No Sponsors yet</p>
+            <a target="_blank" href="https://forms.gle/cW4asHFpJypCDH9D7">
+                <Button type="success">
+                Become a Sponsor
+            </Button>
+                </a>  
+        </div>
+        <Spacer />
       </div>
+
+<Spacer />
+<Divider />
+<footer style={{
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+}}>
+    <img src="/logo.png" alt="KubeHacksLogo" width="40%" />
+    <div style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-evenly"
+    }}>
+        <a href="mailto:josias@josiasw.dev">
+            josias@josiasw.dev
+        </a>
+        <span style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-evenly",
+            alignItems: "center"
+        }}>
+            <img src="/myhackclub.PNG" width="30px" alt="HackClub" />
+            <p >HackClub</p>
+        </span>
+    </div>
+</footer>
+<Spacer />
     </>
   );
 };
